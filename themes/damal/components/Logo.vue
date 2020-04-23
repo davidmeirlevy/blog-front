f7a8045461fc8070d38b8c503b6bbbd656f87d2e
David Meir-Levy <davidmeirlevy@gmail.com>
2020-04-23 10:42:42 +0300

    first damal logo

--- themes/damal/components/Logo.vue
+++ themes/damal/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-	<nuxt-link to="/">{{name}}</nuxt-link>
+	<nuxt-link to="/"><img class="logo" src="/damal.svg" :alt="name"></nuxt-link>
 </template>
 <script>
 	export default {
@@ -14,11 +14,20 @@
 		padding: 10px 0;
 	}
 
+	.logo {
+		width: 150px;
+	}
+
 	@media all and (max-width: 720px) {
 		a {
 			text-align: center;
 			flex: 1;
 			padding: 5px 0;
+		}
+
+		.logo {
+			max-width: 100%;
+			max-height: 100px;
 		}
 	}
 </style>
